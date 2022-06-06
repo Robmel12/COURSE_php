3d0e7a9e3f6e60631af347a3b5364cc1a0ce4da1
Robmel12 <87881619+Robmel12@users.noreply.github.com>
2022-06-06 16:59:03 -0500

    initial commit

---
+++ superglobals/getfile.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $filePath = "uploads/file.txt";
+  $output = file_get_contents($filePath);
+  $ages = explode("\n", $output);
+  echo var_dump($ages);
+  $totalAge = 0;
+  for ($i=0; $i< count($ages); $i++) {
+    $ages[$i] = intval($ages[$i]);
+  }
+  $i=0;
+  foreach ($ages as $age) {
+    echo $age . "<br>";
+  $totalAge = $totalAge + $age;
+  $i++;
+  }
+  echo $i;
+  echo "<br>";
+  echo "The average age is " . ($totalAge / ($i-1));
+  echo "<br>";
+  
+  echo var_dump($ages);
+  ?>
+</body>
+</html>
---
+++ superglobals/putfile.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  if(isset($_POST['submit'])){
+    $myFile= fopen("uploads/file.txt", "a");
+    $txt = $_POST['age'] . "\n";
+    fwrite($myFile, $txt);
+
+  }
+  ?>
+  <form action="putfile.php" method="post">
+    <input type="text" name="age">
+    <input type="submit" name="submit">
+
+  </form>
+</body>
+</html>
